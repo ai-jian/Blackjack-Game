ab814817f621ec7c9fa9f3de3ad20fced97dab33
Ai Jian <53556553+meowwwowo@users.noreply.github.com>
2022-01-20 14:06:38 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const cardArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-let firstCard = 0
-let secondCard = 0
+let player = {
+    name: 'Ai',
+    chips: 200
+}
+let cards = []
 let sum = 0
 let hasBlackjack = false
-let isAlive = true
+let isAlive = false
 let message = ''
 
 const startBtn = document.querySelector('.start-btn')
@@ -11,44 +13,69 @@ const newCardBtn = document.querySelector('.new-card')
 const messageDisplay = document.querySelector('#message')
 const sumDisplay = document.querySelector('#sum')
 const cardsDisplay = document.querySelector('#cards')
+const playerName = document.querySelector('#player-name')
 
-//random cards
-// function getRandomInt(min, max) {
-//     min = Math.ceil(min)
-//     max = Math.floor(max)
-//     return Math.floor(Math.random()*())
-// }
-function startGame() {
 
-    let randomInt = Math.floor(Math.random() * cardArray.length)
-    firstCard = cardArray[randomInt]
-    secondCard = cardArray[randomInt]
-    sum = firstCard + secondCard
+playerName.innerHTML = player.name + ": $" + player.chips
 
-    cardsDisplay.innerHTML = ` ${firstCard} and ${secondCard}`
-    sumDisplay.innerHTML = sum
 
-    if (sum < 21) {
-        message = 'Do you want to draw a new card?'
-    } else if (sum === 21) {
-        message = 'Wohoo! You have got Blackjack!'
-        hasBlackjack = true
+// random cards from 1 - 13
+function getRandomCard() {
+    let randomInt = Math.floor(Math.random() * 13 + 1)
+    if (randomInt > 10) {
+        return 10
+    } else if (randomInt === 1) {
+        return 11
     } else {
-        message = `You're out of the game!`
-        isAlive = false
+        return randomInt
+    }
+}
+
+
+function startGame() {
+    isAlive = true
+    hasBlackjack = false
+    const firstCard = getRandomCard()
+    const secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+
+function renderGame() {
+    cardsDisplay.innerHTML = ''
+    for (let i = 0; i < cards.length; i++) {
+        cardsDisplay.textContent += cards[i] + " "
     }
 
-    messageDisplay.innerHTML = message
+    sumDisplay.textContent = sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        player.chips += 100
+        hasBlackjack = true
+    } else {
+        message = "You're out of the game!"
+        player.chips -= 100
+        isAlive = false
+    }
+    messageDisplay.textContent = message
+    playerName.innerHTML = player.name + ": $" + player.chips
 }
-
 startBtn.addEventListener('click', startGame)
 
-
+//Drawing a new card from the deck!
 function newCard() {
+    if (isAlive === true && hasBlackjack === false) {
+        const card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
 
-
-
-    messageDisplay.innerHTML = `Drawing a new card from the deck!`
 }
-
 newCardBtn.addEventListener('click', newCard)
+
+
